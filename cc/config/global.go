287718902ceb0dcff1bca5cafb444a9fcc732fa4
Vaisakh Murali <mvaisakh@statixos.com>
2023-02-09 17:13:25 +0000

    cc/config: switch to clang 15.0.1
    Change-Id: I92b38574a602b31b1f1d750b085e6025c0f18011
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- cc/config/global.go
+++ cc/config/global.go
@@ -286,8 +286,8 @@ var (
 
 	// prebuilts/clang default settings.
 	ClangDefaultBase         = "prebuilts/clang/host"
-	ClangDefaultVersion      = "clang-r450784d"
-	ClangDefaultShortVersion = "14.0.6"
+	ClangDefaultVersion      = "clang-r458507"
+	ClangDefaultShortVersion = "15.0.1"
 
 	// Directories with warnings from Android.bp files.
 	WarningAllowedProjects = []string{
